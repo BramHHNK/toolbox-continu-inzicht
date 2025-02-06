c79fef0438cca57b43a62da90dd1415b67129405
Koen Stelling <k.stelling@hhnk.nl>
2025-02-06 16:05:12 +0100

    Reformat TCI __init__.py

--- src/toolbox_continu_inzicht/__init__.py
+++ src/toolbox_continu_inzicht/__init__.py
@@ -5,33 +5,32 @@ __version__ = "0.0.3"
 # in alpha release zijn de versie gelijk aan de sprint nummers.
 
 # Hier alleen base functies
-from toolbox_continu_inzicht.base import config
-from toolbox_continu_inzicht.base.config import Config
-from toolbox_continu_inzicht.base import data_adapter
-from toolbox_continu_inzicht.base.data_adapter import DataAdapter
-from toolbox_continu_inzicht.base import fragility_curve
-from toolbox_continu_inzicht.base.fragility_curve import FragilityCurve
+from base import config, data_adapter, fragility_curve
+from base.config import Config
+from base.data_adapter import DataAdapter
+from base.fragility_curve import FragilityCurve
 
 # hier de hoofd modules, sub modules in de mapjes zelf
-from toolbox_continu_inzicht import base
-from toolbox_continu_inzicht import loads
-from toolbox_continu_inzicht import sections
-from toolbox_continu_inzicht import fragility_curves
-from toolbox_continu_inzicht import proof_of_concept
+from toolbox_continu_inzicht import (
+    base,
+    loads,
+    sections,
+    fragility_curves,
+    proof_of_concept,
+)
 
 
 __all__ = [
     "__version__",
     "config",
     "data_adapter",
+    "fragility_curve",
     "Config",
     "DataAdapter",
-    "fragility_curve",
     "FragilityCurve",
+    "base",
     "loads",
     "sections",
-    "proof_of_concept",
-    "base",
-    "sections",
     "fragility_curves",
+    "proof_of_concept",
 ]
